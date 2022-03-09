f6aa092a6cd12966ca235f712e1a1a985bd5d004
plum870131 <R09522620@ntu.edu.tw>
2022-03-09 08:35:25 +0800

    出大事發現公式導錯
    testing cosine ratio can get anything: nope

--- test.py
+++ test.py
@@ -6,44 +6,25 @@ from scipy.optimize import fsolve,root
 import math
 sympy.init_printing()
 
-krot, kpos,led_num,pd_num=2,4,5,6
+x = np.deg2rad(np.arange(0,90,0.5))
+y = np.deg2rad(np.arange(0,90,1))
+xx,yy = np.meshgrid(x,y)
 
-glob_led_pos = np.zeros((2,4,3,5)) #krot kpos 3 led
-pd_pos = np.tile(100*np.arange(0,6),(3,1)) #3 pd
-pd_ori_car = np.tile(100*np.arange(0,6),(3,1)) #3 pd
-led_ori_car = np.tile(100*np.arange(0,5),(3,1)) #3 pd
-out = np.zeros((2,4,5,6))#(krot,kpos,led_num,pd_num)
+# z = np.divide(np.tile(np.cos(x),(y.size,1)).T,np.tile(np.cos(y),(x.size,1)))
+# x x y
+# z=np.tile(np.cos(x),(y.size,1))
+# z = np.tile(np.cos(y),(x.size,1)).T
+z = np.divide(np.tile(np.cos(x),(y.size,1)),np.tile(np.cos(y),(x.size,1)).T)
+print(xx.shape)
+print(z.shape)
 
-for i in range(2):
-    for j in range(4):
-        glob_led_pos[i,j,:,:] = glob_led_pos[i,j,:,:]+(4*i+j)
+fig = plt.figure()
+ax3d = plt.axes(projection="3d")
 
-dis = np.zeros((krot,kpos,led_num,pd_num))
-in_ang = np.zeros((krot,kpos,led_num,pd_num))
-out_ang = np.zeros((krot,kpos,led_num,pd_num))
-
-pos_delta = np.zeros((krot,kpos,led_num,pd_num,3)) #led-pd: pd pointint to led
-
-for pd in range(pd_num):
-    for led in range(led_num):
-        # (x-x)^2 sqrt
-        # glob_led_pos[:,:,:,led]  krotxkposx3
-        pd_extend = np.tile(pd_pos,(krot,kpos,1,1))
-        pos_delta[:,:,led,pd,:] = glob_led_pos[:,:,:,led]-pd_extend[:,:,:,pd]#krotxkposx3
-        dis[:,:,led,pd] = np.sqrt(np.square(pos_delta[:,:,led,pd,:]).sum(axis=2))
-        # in_ang[:,:,led,pd]= 
-
-for pd in range(pd_num):
-    # 計算該pd與all testpoints的角度
-    in_ang[:,:,:,pd] = pos_delta[:,:,:,pd,0]*pd_ori_car[0,pd]+pos_delta[:,:,:,pd,1]*pd_ori_car[1,pd]+pos_delta[:,:,:,pd,2]*pd_ori_car[2,pd]
-for led in range(led_num):
-    out_ang[:,:,led,:] = -pos_delta[:,:,led,:,0]*led_ori_car[0,led] - pos_delta[:,:,led,:,1]*led_ori_car[1,led] - pos_delta[:,:,led,:,2]*led_ori_car[2,led]
-in_ang = np.divide(in_ang,dis)
-out_ang = np.divide(out_ang,dis)
-print(in_ang.shape)
-# print(a)
-# print(a[:,:,:,0])
-
-# print(np.sqrt(np.multiply(a,a)))
-
-
+ax3d = plt.axes(projection='3d')
+ax3d.plot_surface(xx,yy,z, cmap='plasma')
+# ax3d.set_title('Radiant Flux at different distance and angle')
+ax3d.set_xlabel('x')
+ax3d.set_ylabel('y')
+ax3d.set_zlabel('z')
+plt.show()
